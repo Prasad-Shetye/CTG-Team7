db8793f5849000ff93f4ad7fac272e20edaa025c
prathampatel21 <prathampatel@MacBook-Air-66.local>
2024-09-22 14:10:18 -0400

    Added new embedded Calendar feature

--- src/components/public_pages/CalendarComp/cal.js
+++ src/components/public_pages/CalendarComp/cal.js
@@ -1,16 +1,31 @@
-function Cal() {
-        return (
-            <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
-                <iframe 
-                    src="https://embed.styledcalendar.com/#N9R2Q4n8EZIqwlnSenB4" 
-                    title="Styled Calendar" 
-                    className="styled-calendar-container" 
-                    style={{ width: "90%", height: "100vh", border: "none" }} 
-                    data-cy="calendar-embed-iframe">
-                </iframe>
-                <script async type="module" src="https://embed.styledcalendar.com/assets/parent-window.js"></script>
-            </div>
-        );
-    }
+import React, { useEffect } from 'react';
 
-    export default Cal;
+function Cal() {
+    useEffect(() => {
+        // Dynamically load the styled calendar wrapper script
+        const script = document.createElement('script');
+        script.src = "https://embed.styledcalendar.com/assets/parent-window.js";
+        script.async = true;
+        script.type = "module";
+        document.body.appendChild(script);
+
+        return () => {
+            // Clean up script on component unmount
+            document.body.removeChild(script);
+        };
+    }, []);
+
+    return (
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+            <iframe 
+                src="https://embed.styledcalendar.com/#RNkmzR9xP4AF9eUQloqH" 
+                title="Styled Calendar" 
+                className="styled-calendar-container" 
+                style={{ width: "80%", height: "80vh", border: "none" }} 
+                data-cy="calendar-embed-iframe">
+            </iframe>
+        </div>
+    );
+}
+
+export default Cal;
